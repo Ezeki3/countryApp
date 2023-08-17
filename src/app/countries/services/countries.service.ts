df7520e6d140564efea2d0807af5b468a69e194c
Ezequiel Lino <sonisombra@gmail.com>
2023-08-17 17:42:24 -0600

    service

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/Country';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContriesService {
+
+  private apiUrl: string = 'http://restcountries.com/v3.1';
+
+  constructor(
+    private htt: HttpClient
+  ){}
+
+  searchCapital( term:string): Observable<Country[]>{
+    const url = `${this.apiUrl}/capital/${term}`;
+    return this.htt.get<Country[]>(url);
+
+  }
+
+}
