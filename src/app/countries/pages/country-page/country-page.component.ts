2a2429255660d91ac8a425c9497d581a838f69f7
Ezequiel Lino <sonisombra@gmail.com>
2023-08-21 11:03:32 -0600

    creacion del servicio por id

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-country-page',
@@ -10,12 +11,17 @@ import { ActivatedRoute } from '@angular/router';
 export class CountryPageComponent implements OnInit {
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
   ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe( (params) => {
-      console.log({params:params['id']});
+    this.activatedRoute.params.subscribe( ({id}) => {
+      this.countriesService.searchCountryByAlphaCode(id)
+        .subscribe( country =>{
+          console.log({country});
+          
+        })
       
     })
   }
